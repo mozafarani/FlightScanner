2029bed97b0814a4a33bfdaf2f73a00b37eb1e60
Mohammed Alzafarani <mohammefm@gmail.com>
2023-08-25 07:43:08 -0600

    game starting to take shape

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -15,11 +15,46 @@ def checkCols():
 def checkRows():
     print("TODO Later")
 
+# This function checks the player's move
+def checkMove():
+    return True
+
+# This function checks the whole board for a winner
+def checkWinner():
+    return True
+
 
 if __name__ == '__main__':
 
+    print("Welcome to the game of TicTacToe!")
+
     # I would like to have some loop with an odd/even player turn
+    switch = 0
 
-    # I need to check for edge cases like a player making a invalid move
+    gameOn = True
+    validMove = False
 
-    print("First line of code in python!")
+    while gameOn:
+
+        if switch % 2 == 0:
+            print("Player's 1 turn")
+            validMove = checkMove()
+        else:
+            print("Player's 2 turn")
+            validMove = checkMove()
+
+        if validMove:
+
+            # put the X/O in place
+
+            # switch to the other player
+            switch += 1
+
+        if checkWinner():
+            
+            if switch%2 == 0:
+                print("Player 2 is the winner!")
+            else:
+                print("Player 1 is the winner")
+            
+            gameOn = False
