e5833632cd044b1658f3fd23060bad187e5be838
Mohammed Alzafarani <mohammefm@gmail.com>
2023-08-25 07:27:39 -0600

    added outline

--- TicTacToe/main.py
+++ TicTacToe/main.py
@@ -1,1 +1,25 @@
-print("First line of code in python!")
+
+# This function checks the diagonals of the board for a winner
+def checkDiagonals():
+    print("TODO Later")
+
+# This function checks the rows of the board for a winner
+def checkRows():
+    print("TODO Later")
+
+# This function checks the columns of the board for a winner
+def checkCols():
+    print("TODO Later")
+
+# This function prints the board every turn
+def checkRows():
+    print("TODO Later")
+
+
+if __name__ == '__main__':
+
+    # I would like to have some loop with an odd/even player turn
+
+    # I need to check for edge cases like a player making a invalid move
+
+    print("First line of code in python!")
